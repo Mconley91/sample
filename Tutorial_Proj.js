df275f24b245a07b4347712ff046213ac7f7c347
Matt Conley <matthew.e.conley@gmail.com>
2023-11-22 12:13:48 -0500

    added everything else

---
+++ Tutorial_Proj.js
@@ -0,0 +1,6 @@
+const _ = require('lodash');
+const numbers = [1,42,2,34,7,5];
+_.each(numbers,function(number, i){
+console.log(number)
+});
+console.log('this is a test string to see if html can run multiple jscript file');
